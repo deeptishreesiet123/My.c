799b842f80167eeda2ce74ab9832d557ccb07088
deeptishreesiet123 <deeptidodamanidodamani@gmail.com>
2025-05-14 16:18:10 +0530

    Create Factorial.c

---
+++ Factorial.c
@@ -0,0 +1,23 @@
+include<stdio.h>
+int factorial(int i,int fact)
+{
+    if(i==0)
+    {
+        printf("1");
+    }
+    else if(n<0)
+    {
+        printf("enter the positive number");
+    }
+    else
+    {
+        for(i=1;i<=n;i++)
+        fact=i;
+    }
+    return fact;
+    int main()
+    {
+    int c;
+    c=factorial (5,1);
+    }
+}
