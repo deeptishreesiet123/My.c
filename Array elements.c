322fe8b55b8033f322318522a7bada6f98db7ec3
deeptishreesiet123 <deeptidodamanidodamani@gmail.com>
2025-05-15 21:00:09 +0530

    Create Array elements.c

---
+++ Array elements.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int arr[5], i;
+    int *ptr;
+    printf("Enter 5 elements:\n");
+    for(i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+    ptr = arr;
+    printf("Array elements are:\n");
+    for(i = 0; i < 5; i++) {
+        printf("%d ", *(ptr + i));
+    }
+
+    return 0;
+}
